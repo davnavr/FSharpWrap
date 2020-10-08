cd61524aad37fed6da29f4e18fe414aed93acf24
davnavr <davidcalieln@gmail.com>
2020-10-08 12:04:04 -0700

    Add parser for command line arguments

--- src/FSharpWrap.Tool/Arguments.fs
+++ src/FSharpWrap.Tool/Arguments.fs
@@ -1,25 +1,78 @@
 ﻿namespace FSharpWrap.Tool
 
 type Arguments =
-    { AssemblyPaths: string list
-      ProjectPath: string option }
+    { AssemblyPaths: Path * Path list
+      ExcludeNamespaces: string list
+      OutputFile: Path }
+
+    member this.Assemblies =
+        let h, tail = this.AssemblyPaths in h :: tail
 
 [<RequireQualifiedAccess>]
 module Arguments =
-    type private State =
+    type StateType =
+        private
         | AssemblyPaths
+        | Invalid
+        | OutputFile
+        | Unknown
 
-    let rec private parseloop args state =
-        match args with
-        | [] -> fun _ -> state
-        | h :: tail ->
+    type private State =
+        { AssemblyPaths: string list
+          ExcludeNamespaces: string list
+          OutputFile: string
+          Type: StateType }
+
+    [<RequireQualifiedAccess>]
+    module private State = let invalidate st = { st with Type = Invalid }
+
+    let all =
+        [
+            "help", Invalid, "Shows this help message"
+        ]
+        |> Seq.map (fun (name, t, desc) ->
+            let info =
+                {| Description = desc
+                   Name = name
+                   Type = t |}
+            name, info)
+        |> Map.ofSeq
+
+    let parse =
+        let (|Argument|_|) =
             function
-            | AssemblyPaths ->
-                parseloop tail { state with ProjectPath = Some h } AssemblyPaths
-
-    let parse argv =
-        parseloop
-            (List.ofArray argv)
+            | (arg: string) when arg.StartsWith "--" ->
+                let name =
+                    arg
+                        .Substring(2)
+                        .TrimStart()
+                        .TrimEnd()
+                Map.tryFind name all
+            | _ -> None
+        let rec inner state =
+            function
+            | [] ->
+                match state with
+                | { Type = Invalid } -> None
+                | { AssemblyPaths = (Path.Valid phead) :: (Path.ValidList ptail)
+                    OutputFile = Path.Valid out } ->
+                    { AssemblyPaths = phead, ptail
+                      ExcludeNamespaces = state.ExcludeNamespaces
+                      OutputFile = out }
+                    |> Some
+                | _ -> None
+            | arg :: tail ->
+                let state' =
+                    match (arg, state.Type) with
+                    | (Argument arg', _) -> { state with Type = arg'.Type }
+                    | (path, AssemblyPaths) ->
+                        { state with AssemblyPaths = path :: state.AssemblyPaths }
+                    | (path, OutputFile) ->
+                        { state with OutputFile = path; Type = Unknown }
+                    | _ -> State.invalidate state
+                inner state' tail
+        inner
             { AssemblyPaths = []
-              ProjectPath = None }
-            AssemblyPaths
+              ExcludeNamespaces = []
+              OutputFile = ""
+              Type = Unknown }
--- src/FSharpWrap.Tool/Program.fs
+++ src/FSharpWrap.Tool/Program.fs
@@ -1,8 +1,19 @@
 ﻿[<RequireQualifiedAccess>]
 module FSharpWrap.Tool.Program
 
+let private help =
+    [
+        "Usage: fsharpwrap [--help]"
+        ""
+        "Options:"
+        for arg in Arguments.all do
+            sprintf "    --%s %s" arg.Key arg.Value.Description // TODO: Align descriptions.
+    ]
+
 [<EntryPoint>]
 let main argv =
-    let args = Arguments.parse argv
-    printfn "Hello World!"
-    0
+    match List.ofArray argv |> Arguments.parse with
+    | Some args -> 0
+    | None ->
+        List.iter (printfn "%s") help
+        -1
