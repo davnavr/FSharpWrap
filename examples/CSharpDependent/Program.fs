9f2ce3a57a75bee294fd8d0fec788f29e8825fa8
davnavr <davidcalieln@gmail.com>
2020-11-07 11:26:05 -0800

    Add C# project to test code generation for project references

---
+++ examples/CSharpDependent/Program.fs
@@ -0,0 +1,11 @@
+﻿module Program
+
+open CSharpDependency
+
+[<EntryPoint>]
+let main _ =
+    let msg = MyString.ofString "Hello"
+    msg
+    |> MyString.value
+    |> printfn "%s"
+    0 // return an integer exit code
